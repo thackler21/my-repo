c9b661d7728ae6063565de550c0a89c98bc22bc9
Andreas Pal <andreas.pal1979@gmail.com>
2022-11-11 13:58:38 +0100

    auf dem Main-Branch ein Commit

--- new_shell.sh
+++ new_shell.sh
@@ -4,3 +4,7 @@
 # Einfügen der 4ten Zeile
 # beginn der Ansible-Reihe
 # neue Zeile per Web hinzugefügt
+<<<<<<< 1bbbab226c13779e222234d8e5f27efd0f09954c
+=======
+# auf dem main Branch ein neues Commit1
+>>>>>>> auf dem Main-Branch ein Commit
